aed2e6de15f43290e3f9c35c2af53f1ffe8a2c4b
user530 <m.timarsuev@gmail.com>
2024-05-15 18:56:37 +0300

    Added amoCrm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { amoConfig } from './config/amoCRM.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [amoConfig],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
