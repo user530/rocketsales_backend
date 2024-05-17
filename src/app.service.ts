64285660712a3ca020394f1a4d99168043d1c145
user530 <m.timarsuev@gmail.com>
2024-05-17 14:37:53 +0300

    Updated service building from the scratch

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,7 +9,8 @@ import {
   GetLeadsResponse,
   GetContactsResponse,
   GetPipelinesResponse,
-  GetUsersResponse
+  GetUsersResponse,
+  JoinedLeads,
 } from './types';
 
 @Injectable()
@@ -18,86 +19,42 @@ export class AppService {
     private readonly configService: ConfigService,
   ) { }
 
-  async fetchLeads(query?: Record<string, string>) {
-    const leads = await this.getLeads(query);
-
-    // Prepare containers
-    const responsibleIds = new Set<number>();
-    const pipelineIds = new Set<number>();
-    const statusIds = new Set<number>();
-    const contactIds = new Set<number>();
-
-    // Iterate over leads and collect required data
-    leads.forEach(
-      ({ responsible_user_id, pipeline_id, status_id, _embedded: { contacts } }) => {
-        // Store the data in the associated set
-        responsibleIds.add(responsible_user_id);
-        pipelineIds.add(pipeline_id);
-        statusIds.add(status_id);
-        // Add each contact in the contacts array
-        contacts.forEach(contact => contactIds.add(contact.id));
-      }
-    )
-
-    console.log('Responsible IDs'); console.log(responsibleIds);
-    console.log('Pipelines IDs'); console.log(pipelineIds);
-    console.log('Contact IDs'); console.log(contactIds);
-    console.log('Status IDs'); console.log(statusIds);
-
-    const responsibleUsers = await this.getResponsible(Array.from(responsibleIds));
-
-    const contacts = await this.getContacts(Array.from(contactIds));
-
-    return leads;
-  }
-
-  async getLeads(query?: Record<string, string>): Promise<Lead[]> {
-    const url = new URL(`${this.configService.get<string>('AMOCRM_API_URL')}/api/v4/leads`);
-
-    // Add 'with=contacts' param by default
-    url.searchParams.append('with', 'contacts');
-
-    // Add query params
-    if (query) {
-      Object
-        .keys(query)
-        .forEach(paramName => url.searchParams.append(paramName, query[paramName]));
-    }
-
-    try {
-      // Fetch leads adding token
-      const response = await axios.get<GetLeadsResponse>(
-        url.toString(),
-        {
-          headers: {
-            Authorization: `Bearer ${this.configService.get<string>('AMOCRM_API_TOKEN')}`,
-          },
-        }
-      );
-
-      // Return empty object if there is no content (?)
-      if (response.status === 204)
-        return [];
-
-      return response.data._embedded.leads;
-    } catch (error) {
-      console.error('Failed to fetch leads from the amoCRM', error);
-      throw new Error('Failed to fetch leads');
-    }
-  }
-
-  async getStatuses(): Promise<Status[]> {
-    // Fetch pipelines and statuses
+  async getJoinedLeads(): Promise<JoinedLeads[]> {
+    const leads: Lead[] = await this.getLeadsWithContacts();
+    const { statusIds, responsibleIds, contactIds } = this.leadsToRelationIds(leads);
+    const statuses: Status[] = await this.getStatusesByIds(statusIds);
+    const responsible: User[] = await this.getResponsiblesByIds(responsibleIds);
+    const contacts: Contact[] = await this.getContactsByIds(contactIds);
+    const joinedLeads: JoinedLeads[] = this.joinEntities();
     return [];
   }
 
-  async getResponsible(responsibleIds: number[]): Promise<User[]> {
-    // Fetch all users
+  private async getLeadsWithContacts(): Promise<Lead[]> {
     return [];
-  }
+  };
 
-  async getContacts(contactIds: number[]): Promise<Contact[]> {
-    // Fetch all contacts
+  private async getStatusesByIds(statusIds: string[]): Promise<Status[]> {
     return [];
-  }
+  };
+
+  private async getResponsiblesByIds(userIds: string[]): Promise<User[]> {
+    return [];
+  };
+
+  private async getContactsByIds(contactIds: string[]): Promise<Contact[]> {
+    return [];
+  };
+
+  private leadsToRelationIds(leads: Lead[]): Record<'statusIds' | 'responsibleIds' | 'contactIds', string[]> {
+    return {
+      statusIds: [],
+      responsibleIds: [],
+      contactIds: [],
+    };
+  };
+
+  private joinEntities(): JoinedLeads[] {
+    return [];
+  };
+
 }
