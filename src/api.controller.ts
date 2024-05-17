aaa508cf2ea584c8651278201ba02b0ca0abfcc5
user530 <m.timarsuev@gmail.com>
2024-05-18 00:28:41 +0300

    Updeted Api controller

--- src/api.controller.ts
+++ src/api.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiService } from './services';
+import { JoinedLeads } from './types';
+
+interface IApiController {
+  getLeads(query?: string): Promise<JoinedLeads[]>
+}
 
 @Controller('api')
-export class ApiController {
+export class ApiController implements IApiController {
   constructor(
     private readonly appService: ApiService
   ) { }
 
   @Get('leads')
-  async getLeads(@Query('query') query: string): Promise<any> {
-    console.log(query);
+  async getLeads(@Query('query') query?: string): Promise<JoinedLeads[]> {
     return this.appService.getJoinedLeads(query);
   }
 }
